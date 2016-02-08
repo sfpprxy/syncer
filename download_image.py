e78b0592e5db8b22e3435aba3b5ce285afb6105b
Joe <rxyjoey@gmail.com>
2016-02-08 16:07:41 +0000

    Remove test files after 1.0 implemented

--- download_image.py
+++
@@ -1,9 +0,0 @@
-import random
-import urllib.request
-
-def download_web_image(url):
-    image_name = random.randrange(1, 100)
-    full_name = str(image_name) + ".pdf"
-    urllib.request.urlretrieve(url, full_name)
-
-download_web_image("https://cumoodle.coventry.ac.uk/pluginfile.php/1159097/mod_resource/content/10/M19COM/Lectures/Overview_M19COM_2014_15_Sem4.pdf")
